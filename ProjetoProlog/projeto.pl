b4672bc7715db6a3a16d25dd183e454fe2d72795
Bruno Pedroso <pedrosoagt@gmail.com>
2017-06-08 17:07:29 -0300

    4 done

--- ProjetoProlog/projeto.pl
+++ ProjetoProlog/projeto.pl
@@ -3,12 +3,13 @@
 %Murilo Santana 12662185
 
 %Voos
-%@params (origem, destino, código voo, horário, (chegadadia, horário), n conexoes, companhia, [dias que o voo acontece])
+%@params (origem,destino,códigoVoo,horário,(chegadadia,horário),numConexoes,companhia,[dias que o voo acontece]).
 
 %partida de sao_paulo
 	voo(sao_paulo, mexico,gl1,8:25,(mesmo,20:25),0,gol,[qua,sex,dom]).
 	voo(sao_paulo, nova_york,gl2,8:25,(mesmo,20:25),0,gol,[qua,sex,dom]).
 	voo(sao_paulo, nova_york,gl2,7:10,(mesmo,20:25),0,gol,[seg,qua,sab]).
+	voo(sao_paulo, nova_york,gl2,8:25,(seguinte,20:24),0,gol,[seg,qua,sab]).
 	voo(sao_paulo, nova_york,gl2,7:10,(mesmo,20:25),3,gol,[seg,qua,sab]).
 	voo(sao_paulo, londres,gl3,8:25,(mesmo,20:25),0,gol,[qua,sex,dom]).
 	voo(sao_paulo, lisboa,gl4,8:25,(mesmo,20:25),0,gol,[qua,sex,dom]).
@@ -65,33 +66,55 @@
 %@params (origem, destino, companhia, dia da semana, horário)
 
 	voodireto(O, D, E, W, H):- 
-		voo(O, D,_,H,(_,_),0,E,[W,_,_]); 
-		voo(O, D,_,H,(_,_),0,E,[_,W,_]); 
+		voo(O, D,_,H,(_,_),0,E,[W,_,_]);	 
+		voo(O, D,_,H,(_,_),0,E,[_,W,_]);	
 		voo(O, D,_,H,(_,_),0,E,[_,_,W]).
 
 %conexoes
 %@%params (origem, destino)
 %pergunta roteiro(Origem, Destino, ListaVoos).
+
+	/*voodireto(H, Y), diferente(X, H), diferente(X, Y), diferente(Y, H).
+	diferente(X, Y):- X \= Y.*/
 	
-	conexoes(X, Y):- voodireto(X,H), voodireto(H, Y), diferente(X, H), diferente(X, Y), diferente(Y, H).
-	diferente(X, Y):- X \= Y.
-	
-%voo direto  dia da semana
+%voos direto  dia da semana
 %pergunta filtra_voo_dia_semana(Origem,Destino, DiaSemana, HorarioSaida,HorarioChegada,Companhia).
 	
 	filtra_voo_dia_semana(O, D, W, S, C, E):- 
 		voo(O, D,_,S,(_,C),0,E,[W,_,_]); 
-		voo(O, D,_,S,(_,C),0,E,[_,W,_]); 
+		voo(O, D,_,S,(_,C),0,E,[_,W,_]); 	
 		voo(O, D,_,S,(_,C),0,E,[_,_,W]).
 
-%vDo direto menor duracao
+%voos direto menor duracao
 %@params (origem, destino, dia da semana)
 %pergunta menorDuracao(Origem,Destino,dia,HorarioSaida,HorarioChegada,Companhia).
 	
+	menorDuracao(O,D,W,S,C,E):- 
+		setof([T,S,C,E,W],menorduracaoaux(O,D,_,S,(_,C),_,E,W,T),L), 
+		comecolista(L,[_,S,C,E,W]).
 
+	menorduracaoaux(O,D,Cod,Hora:Min,(_,HoraC:MinC),_,Comp,Dia,Duração):-
+		voo(O,D,Cod,Hora:Min,(DiaC,HoraC:MinC),_,Comp,Dias),
+		contem(Dias,Dia),soma60(Hora,Min,DuraçãoS),
+		soma60(HoraC,MinC,DuraçãoC),soma24(DiaC,DuraçãoC,DuraçãoC2),
+		numero(DuraçãoC2,DuraçãoS,Duração).
 
-%teste de print 
-	show_records([]).
-	show_records([A|B]) :-
-	  format('Origem = ~w\tName = ~w\tGrade = ~w~n',A),
-	  show_records(B).
+%voos diretos calculando a duração
+%@params (Origem, Destino dia semana)
+%pergunta (roteiro(Origem, Destino, DiaSaída, HorSaida, Duração).
+
+%auxiliares
+	contem([X|_],X).
+	contem([_|Cauda],X):- contem(Cauda,X).
+
+	comecolista([L|_],L).
+
+	numero(X,0,X).
+	numero(0,Y,Y).
+	numero(X,Y,C):- X1 is X-1,Y1 is Y-1,numero(X1,Y1,C),!.
+
+	soma60(0,Y,Y).
+	soma60(X,Y,C):- X1 is X-1,Y1 is Y+60, soma60(X1,Y1,C),!.
+
+	soma24(mesmo,Y,Y).
+	soma24(seguinte,Y,C):- C is Y + 1440,!.
