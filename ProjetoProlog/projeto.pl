626691390c4268a3ea06bca15aa61e7b50282798
Bruno Pedroso <pedrosoagt@gmail.com>
2017-06-09 13:15:26 -0300

    2 and 5 done

--- ProjetoProlog/projeto.pl
+++ ProjetoProlog/projeto.pl
@@ -11,7 +11,7 @@
 	voo(sao_paulo, mexico,gl1,8:25,(mesmo,20:25),0,gol,[qua,sex,dom]).
 	voo(sao_paulo, nova_york,gl2,8:25,(mesmo,20:25),0,gol,[qua]).
 	voo(sao_paulo, nova_york,gl2,7:10,(mesmo,20:25),0,gol,[seg,qua,sab]).
-	voo(sao_paulo, nova_york,gl2,8:25,(seguinte,20:24),0,gol,[seg,qua]).
+	voo(sao_paulo, nova_york,bla,8:30,(mesmo,20:25),0,gol,[seg,qua]).
 	voo(sao_paulo, nova_york,gl2,7:10,(mesmo,20:25),3,gol,[seg,qua,sab]).
 	voo(sao_paulo, londres,gl3,8:25,(mesmo,12:25),0,gol,[qua,sex,dom]).
 	voo(sao_paulo, lisboa,gl4,8:25,(mesmo,20:25),0,gol,[qua,sex,dom]).
@@ -74,8 +74,14 @@
 %conexoes
 %pergunta roteiro(Origem, Destino, ListaVoos).
 
-	roteiro(O,D):- voodireto(O,D,_,_,_),!.
-	roteiro(O,D):- voodireto(O,Z,_,_,_),roteiro(Z,D).
+	roteiro(O,D,L):-
+		\+ aux(O,D),
+		voo(O,X,C1,_,_,_,_,_),
+		voo(X,D,C2,_,_,_,_,_),
+		conc([C1],[C2],L).
+
+	aux(O,D):-
+	voo(O,D,_,_,_,_,_,_),!.
 
 %voos direto  dia da semana
 %pergunta filtra_voo_dia_semana(Origem,Destino, DiaSemana, HorarioSaida,HorarioChegada,Companhia).
@@ -88,36 +94,56 @@
 %pergunta menorDuracao(Origem,Destino,dia,HoraSaida,HoraChegada,Companhia).
 
 	menorDuracao(O,D,W,S,C,E):-
-		setof([T,S,C,E,W],menorduracaoaux(O,D,_,S,(_,C),_,E,W,T),L),
+		setof([T,S,C,E,W],auxMenorDuracao(O,D,_,S,(_,C),_,E,W,T),L),
 		inicioLista(L,[_,S,C,E,W]).
 
-	menorduracaoaux(O,D,Cod,H:M,(_,HoraC:MinC),_,Comp,Dia,Duração):-
-		voo(O,D,Cod,Hora:Min,(DiaC,HoraC:MinC),_,Comp,Dias),
-		membro(Dias,Dia),soma60(Hora,Min,DuraçãoS),
-		soma60(HoraC,MinC,DuraçãoC),soma24(DiaC,DuraçãoC,DuraçãoC2),
-		numero(DuraçãoC2,DuraçãoS,Duração).
+	auxMenorDuracao(O,D,_,Hora:Min,(_,HoraC:MinC),_,E,Dia,Duracao):-
+		voo(O,D,_,Hora:Min,(DiaC,HoraC:MinC),0,E,Dias),
+		membro(Dias,Dia),
+		somaHora(Hora,Min,DuracaoS),
+		somaHora(HoraC,MinC,DuracaoC),
+		somaDia(DiaC,DuracaoC,DuracaoC2),
+		numero(DuracaoC2,DuracaoS,Duracao).
 
 %voos diretos calculando a duração
 %pergunta roteiro(Origem, Destino, DiaSaída, HoraSaida, Duração).
+
+	roteiro(O, D, DiaS, Hora:Min, Duracao):-
+		menorDuracao(O, D,_,_,_,_),
+		voo_dirireto(O,D,_,DiaS,Hora:Min,HoraC:MinC,DiaC),
+		somaHora(Hora,Min,DuracaoS),
+		somaHora(HoraC,MinC,DuracaoC),
+		somaDia(DiaC,DuracaoC,DuracaoC2),
+		numero(DuracaoC2,DuracaoS,Duracao),!.
+
+	voo_dirireto(O, D, E, W, Hora:Min, HoraC:MinC, DiaC):-
+		voo(O,D,_,Hora:Min,(DiaC,HoraC:MinC),0,E,T),
+		membro(T, W).
 
 %auxiliares
 	membro([X|_],X).
 	membro([_|T],X):-
 		membro(T,X).
 
+	diferente(X,Y):- X \= Y. %função não mais usada
+
+	conc([],L,L).
+	conc([X|L1],L2,[X|L]) :-
+		conc(L1,L2,L).
+
 	inicioLista([L|_],L).
+
+	somaHora(0,Y,Y).
+	somaHora(X,Y,C):-
+		X1 is X-1,Y1 is Y+60,
+		somaHora(X1,Y1,C),!.
+
+	somaDia(mesmo,Y,Y).
+	somaDia(seguinte,Y,C):-
+		C is Y + 1440,!.
 
 	numero(X,0,X).
 	numero(0,Y,Y).
 	numero(X,Y,C):-
 		X1 is X-1,Y1 is Y-1,
 		numero(X1,Y1,C),!.
-
-	soma60(0,Y,Y).
-	soma60(X,Y,C):-
-		X1 is X-1,Y1 is Y+60,
-		soma60(X1,Y1,C),!.
-
-	soma24(mesmo,Y,Y).
-	soma24(seguinte,Y,C):-
-		C is Y + 1440,!.
